d1c1e22f91aabe214e3f1e321ef3d34786b34ced
kvv@kmd.dk <kvv@kmd.dk>
2025-05-18 18:41:46 +0200

    add re_deploy script

---
+++ re_deploy.sh
@@ -0,0 +1,19 @@
+echo "Building the new docker container..."
+if docker build -t madplanlaegger:latest .; then
+    echo "Docker container built successfully."
+else
+    echo "Failed to build the Docker container."
+    exit 1
+fi
+
+echo "Stopping and removing the old container..."
+docker stop madplanlaegger
+docker rm madplanlaegger
+
+echo "Running the new container..."
+if docker run -d --name madplanlaegger -p 8050:8050 madplanlaegger:latest; then
+    echo "New container is running."
+else
+    echo "Failed to run the new container."
+    exit 1
+fi
